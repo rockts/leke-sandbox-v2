c92c349a6e029e7766678673889506300163f632
rockts <rockts@sina.com>
2018-05-22 03:20:38 +0800

    准备商品订单信息

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -1,10 +1,37 @@
 'use strict'
 
 const logger = use('App/Services/Logger')
+const Config = use('Config')
+const moment = use('moment')
 
 class CheckoutController {
   render ({ view }) {
-    logger.debug('debug log')
+    // 公众账号 ID
+    const appid = Config.get('wxpay.appid')
+
+    // 商户号
+    const mch_id = Config.get('wxpay.mch_id')
+
+    // 密钥
+    const key = Config.get('wxpay.key')
+
+    // 商户订单号
+    const out_trade_no = moment().local().format('YYYYMMDDHHmmss')
+
+    // 商品描述
+    const body = 'lekeopen'
+
+    // 支付价格
+    const total_fee =3
+
+    // 支付类型
+    const trade_type = 'NATIVE'
+
+    // 商品 ID
+    const product_id = 1
+
+    //
+
     return view.render('commerce.checkout')
   }
 }
