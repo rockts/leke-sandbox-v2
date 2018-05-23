808859771941fb6d2c87a6f971b6c2f5ecf3eceb
rockts <rockts@sina.com>
2018-05-24 00:15:50 +0800

    请求统一下单接口返回跳转链接

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -8,9 +8,105 @@ const queryString  = use('querystring')
 const crypto       = use('crypto')
 const convert      = use('xml-js')
 const axios        = use('axios')
-const qrcode       = use('qrcode')
 
 class CheckoutController {
+  async pay ({ request }) {
+    logger.info('请求支付 ----------------------------------')
+
+    // 公众账号 ID
+    const appid = Config.get('wxpay.appid')
+
+    // 商户号
+    const mch_id = Config.get('wxpay.mch_id')
+
+    // 密钥
+    const key = Config.get('wxpay.key')
+
+    // 商户订单号
+    const out_trade_no = moment().local().format('YYYYMMDDHHmmss')
+
+    // 商品描述
+    const body = 'LekeOpen'
+
+    // 商品价格
+    const total_fee = 3
+
+    // 支付类型
+    const trade_type = 'MWEB'
+
+    // 用户 IP
+    const spbill_create_ip = request.header('x-real-ip')
+
+    // 商品 ID
+    const product_id = 1
+
+    // 通知地址
+    const notify_url = Config.get('wxpay.notify_url')
+
+    // 随机字符
+    const nonce_str = randomString.generate(32)
+
+    // 统一下单接口
+    const unifiedOrderApi = Config.get('wxpay.api.unifiedorder')
+
+    let order = {
+      appid,
+      mch_id,
+      out_trade_no,
+      body,
+      total_fee,
+      trade_type,
+      product_id,
+      notify_url,
+      nonce_str,
+      spbill_create_ip
+    }
+
+    const sign = this.wxPaySign(order, key)
+
+    const xmlOrder = this.orderToXML(order, sign)
+
+    // 调用统一下单接口
+    const wxPayResponse = await axios.post(unifiedOrderApi, xmlOrder)
+
+    const data = this.xmlToJS(wxPayResponse.data)
+
+    logger.debug(data)
+
+    return 'ok'
+  }
+
+  xmlToJS (xmlData) {
+    const _data = convert.xml2js(xmlData, {
+      compact: true,
+      cdataKey: 'value',
+      textKey: 'value'
+    }).xml
+
+    const data = Object.keys(_data).reduce((accumulator, key) => {
+      accumulator[key] = _data[key].value
+      return accumulator
+    }, {})
+
+    return data
+  }
+
+  orderToXML (order, sign) {
+    order = {
+      xml: {
+        ...order,
+        sign
+      }
+    }
+
+    // 转换成 xml 格式
+    const xmlOrder = convert.js2xml(order, {
+      compact: true
+    })
+
+    return xmlOrder
+  }
+
   wxPaySign (data, key) {
     // 1. 排序
     const sortedOrder = Object.keys(data).sort().reduce((accumulator, key) => {
@@ -34,95 +130,10 @@ class CheckoutController {
   }
 
   async render ({ view }) {
-    // 公众账号 ID
-    const appid = Config.get('wxpay.appid')
 
-    // 商户号
-    const mch_id = Config.get('wxpay.mch_id')
 
-    // 密钥
-    const key = Config.get('wxpay.key')
 
-    // 商户订单号
-    const out_trade_no = moment().local().format('YYYYMMDDHHmmss')
-
-    // 商品描述
-    const body = 'LekeOpen'
-
-    // 商品价格
-    const total_fee = 3
-
-    // 支付类型
-    const trade_type = 'NATIVE'
-
-    // 商品 ID
-    const product_id = 1
-
-    // 通知地址
-    const notify_url = Config.get('wxpay.notify_url')
-
-    // 随机字符
-    const nonce_str = randomString.generate(32)
-
-    // 统一下单接口
-    const unifiedOrderApi = Config.get('wxpay.api.unifiedorder')
-
-    let order = {
-      appid,
-      mch_id,
-      out_trade_no,
-      body,
-      total_fee,
-      trade_type,
-      product_id,
-      notify_url,
-      nonce_str
-    }
-
-    const sign = this.wxPaySign(order, key)
-
-    order = {
-      xml: {
-        ...order,
-        sign
-      }
-    }
-
-    // 转换成 xml 格式
-    const xmlOrder = convert.js2xml(order, {
-      compact: true
-    })
-
-    // 调用统一下单接口
-    const wxPayResponse = await axios.post(unifiedOrderApi, xmlOrder)
-
-    const _prepay = convert.xml2js(wxPayResponse.data, {
-      compact: true,
-      cdataKey: 'value',
-      textKey: 'value'
-    }).xml
-
-    const prepay = Object.keys(_prepay).reduce((accumulator, key) => {
-      accumulator[key] = _prepay[key].value
-      return accumulator
-    }, {})
-
-    // 生成二维码链接
-    const qrcodeUrl = await qrcode.toDataURL(prepay.code_url, { width: 300 })
-
-    // logger.debug(prepay)
-
-    // logger.info('预支付响应：', wxPayResponse)
-
-    // logger.debug(xmlOrder)
-
-    // logger.info('签名：', sign)
-
-    // logger.debug(stringOrder)
-
-    // logger.debug(sortedOrder)
-
-    return view.render('commerce.checkout', { qrcodeUrl })
+    return view.render('commerce.checkout')
   }
 
   wxPayNotify ({ request }) {
